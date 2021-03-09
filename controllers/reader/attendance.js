c7665c99c4ef8d11455e762674a8055706b476d0
Barry Bear <wokhug99@gmail.com>
2021-03-09 09:25:40 +0700

    Fix bug of post attendance

--- controllers/reader/attendance.js
+++ controllers/reader/attendance.js
@@ -18,44 +18,45 @@ exports.checkAttendance = async (req, res, next) => {
       throw createError('Course not found', 404);
 
     const student = await Student.findOne({ rfidTag });
-    if (!student)
-      throw createError('Student not found', 404);
-
-    const studentInCourse = course.regStudentIds.includes(student._id);
-    if (studentInCourse) {
-      const todayAtZero = new Date().setHours(0, 0, 0, 0);
-      const existingAttendance = await Attendance.findOne({
-        studentId: student._id,
-        courseId: course._id,
-        createdAt: { $gt: todayAtZero }
-      });
-      if (existingAttendance) {
-        existingAttendance.checkTimes.push(new Date());
-        await existingAttendance.save();
-      } else {
-        const attendance = new Attendance({
-          courseId: course._id,
+    if (student) {
+      const studentInCourse = course.regStudentIds.includes(student._id);
+      if (studentInCourse) {
+        const todayAtZero = new Date().setHours(0, 0, 0, 0);
+        const existingAttendance = await Attendance.findOne({
           studentId: student._id,
-          checkTimes: [new Date()]
+          courseId: course._id,
+          createdAt: { $gt: todayAtZero }
         });
-        await attendance.save();
+        if (existingAttendance) {
+          existingAttendance.checkTimes.push(new Date());
+          await existingAttendance.save();
+        } else {
+          const attendance = new Attendance({
+            courseId: course._id,
+            studentId: student._id,
+            checkTimes: [new Date()]
+          });
+          await attendance.save();
+        }
+      } else {
+        // to different model
+        console.log("Student does not registered for this course :D");
       }
-    } else {
-      // to different model
-      console.log("Student does not registered for this course :D");
-    }
 
-    io.getIO().emit('attendance', {
-      action: 'create',
-      studentName: student.name
-    });
-    res.status(201).json({
-      message: studentInCourse ?
-        'Check attendance successfully :D' :
-        'Student does not registered for this course :D',
-      course: course._id,
-      student: student.name
-    });
+      io.getIO().emit('attendance', {
+        action: 'create',
+        studentName: student.name
+      });
+      res.status(201).json({
+        message: studentInCourse ?
+          'Check attendance successfully :D' :
+          'Student does not registered for this course :D',
+        course: course._id,
+        student: student.name
+      });
+    } else {
+      res.status(200).json({ message: "No student data :D" });
+    }
   } catch (error) {
     checkStatusCode(error, next);
   }
@@ -65,7 +66,7 @@ exports.checkAttendanceByRoom = async (req, res, next) => {
   const { rfidTag, roomCode } = req.query;
 
   try {
-    
+
   } catch (error) {
     checkStatusCode(error, next);
   }
